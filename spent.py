237ee0db7a8cd64b6f25c46cff9ba4045d9e001c
hamayelq <qureshihamayel@gmail.com>
2020-05-25 20:21:28 -0400

    Added new functionality to spent.py

--- spent.py
+++ spent.py
@@ -1,4 +1,5 @@
 import sqlite3 as db
+from datetime import datetime
 
 
 def init():
@@ -16,4 +17,58 @@ def init():
     connect.commit()
 
 
-init()
+def log(amount, category, message=""):
+    date = str(datetime.now())
+    connect = db.connect("spent.db")
+    cursor = connect.cursor()
+    SQL = '''
+    INSERT INTO EXPENSES VALUES (
+         {},
+        '{}',
+        '{}',
+        '{}'
+    )
+    '''.format(amount, category, message, date)
+    cursor.execute(SQL)
+    connect.commit()
+
+
+def delete(amount, category, message=""):
+    connect = db.connect("spent.db")
+    cursor = connect.cursor()
+    SQL = '''
+    DELETE FROM EXPENSES WHERE Amount = {} AND Category = '{}' AND MESSAGE = '{}'
+    '''
+    try:
+        cursor.exequte(SQL)
+    except:
+        print("Please enter all fields. Try again!")
+    connect.commit()
+
+
+def view(category=None):
+    connect = db.connect("spent.db")
+    cursor = connect.cursor()
+    if category:
+        SQL = '''
+        SELECT * FROM EXPENSES WHERE Category = '{}'
+        '''.format(category)
+        SQLtotal = '''
+        SELECT SUM(Amount) FROM EXPENSES WHERE Category = '{}'
+        '''.format(category)
+    else:
+        SQL = '''
+        SELECT * FROM EXPENSES
+        '''
+        SQLtotal = '''
+        SELECT SUM(Amount) FROM EXPENSES WHERE Category = '{}'
+        '''.format(category)
+    cursor.execute(SQL)
+    results = cursor.fetchall()
+    cursor.execute(SQLtotal)
+    totalAmount = cursor.fetchone()[0]
+
+    return totalAmount, results
+
+
+print(view('gas'))
